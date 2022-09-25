74486216ad6e3280b3813d8c632249c575e0b22f
Juan Dario Rodas <jdrodas@hotmail.com>
2022-09-25 18:40:57 -0500

    Cambio de framework a .NET 4.8.1

--- SiembrasCorantioquia_PoC_Console/SiembrasCorantioquia_PoC_Console/Program.cs
+++ SiembrasCorantioquia_PoC_Console/SiembrasCorantioquia_PoC_Console/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Siembras Corantioquia - PoC para conectividad a SQLite");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiembrasCorantioquia_PoC_Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Siembras Corantioquia - PoC para conectividad a SQLite");
+        }
+    }
+}
+
